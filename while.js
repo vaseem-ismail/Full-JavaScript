ea079950cd3efbdb9f6db3777cbc367cee18525d
vaseem-ismail <canvas9787839798@gmail.com>
2024-09-09 11:47:08 +0530

    9-9-24

---
+++ while.js
@@ -0,0 +1,6 @@
+let i="I am sorry";
+while(i=="I am sorry")
+    {
+    console.log(i);
+    // i++;
+    }
---
+++ while2.js
@@ -0,0 +1,5 @@
+let count=5;
+while(count>=1){
+    console.log(`count is: ${count}`);
+    count--;
+}
